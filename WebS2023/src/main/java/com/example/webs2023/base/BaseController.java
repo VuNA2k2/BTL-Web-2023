3d95ce15de843e6b7da29f5eac5a63cf99475c46
VuNAP <vuvovan2k2>
2023-03-08 00:18:58 +0700

    [EDIT] init base controller

--- WebS2023/src/main/java/com/example/webs2023/base/BaseController.java
+++ WebS2023/src/main/java/com/example/webs2023/base/BaseController.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-//public class BaseController<S extends BaseService> extends HttpServlet {
-//    public final S service = (S) S.getInstance();
-//}
+public class BaseController extends HttpServlet {
+
+}
 
