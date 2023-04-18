0c2b3fd104f948ff08601e48cd3d97fc538bedce
VuNAP <vuvovan2k2>
2023-04-18 21:55:32 +0700

    category restful

---
+++ WebS2023/src/main/java/com/example/webs2023/controller/CategoryController.java
@@ -0,0 +1,49 @@
+package com.example.webs2023.controller;
+
+import com.example.webs2023.base.BaseController;
+import com.example.webs2023.base.DependencyInjector;
+import com.example.webs2023.base.Response;
+import com.example.webs2023.service.category.CategoryService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet(value = "/categories")
+public class CategoryController extends BaseController {
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        service = DependencyInjector.getDependency(CategoryService.class);
+    }
+
+    @Override
+    protected Response getMethod(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            if(request.getParameter("id") != null && !request.getParameter("id").isEmpty()) {
+                return new Response("success", "Thanh Cong", service.getById(Long.parseLong(request.getParameter("id"))));
+            } else {
+                return new Response("success", "Thanh Cong", service.getAll());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Response("fail", "That bai", e.getStackTrace());
+        }
+    }
+
+    @Override
+    protected Response postMethod(HttpServletRequest request, HttpServletResponse response) {
+        return null;
+    }
+
+    @Override
+    protected Response putMethod(HttpServletRequest request, HttpServletResponse response) {
+        return null;
+    }
+
+    @Override
+    protected Response deleteMethod(HttpServletRequest request, HttpServletResponse response) {
+        return null;
+    }
+}
