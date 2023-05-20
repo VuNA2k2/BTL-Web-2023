f247409c85d50b108b3f71a362ef83f6b961dcf0
Phạm Huy Khôi <108706249+HKPH@users.noreply.github.com>
2023-05-20 17:28:22 +0700

    Update Order.sql

--- WebS2023/src/main/resources/Order.sql
+++ WebS2023/src/main/resources/Order.sql
@@ -1,5 +1,7 @@
 CREATE TABLE public.orders (
     id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES users(id),
-    total_money DECIMAL(10, 2)
+    total_money DECIMAL(10, 2),
+    status VARCHAR(255)
 );
+
