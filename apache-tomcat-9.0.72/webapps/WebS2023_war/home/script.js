9bb2c61c30bcf8c56ff436bb5cedfcd32bee0893
VuNAP <VuNAProPTIT@gmail.com>
2023-05-22 23:22:02 +0700

    routing

---
+++ apache-tomcat-9.0.72/webapps/WebS2023_war/home/script.js
@@ -0,0 +1,16 @@
+function checkLogged(role) {
+    let token;
+    document.cookie.split(';').forEach(function (c) {
+        if (c.includes('token')) {
+            token = c.split('=')[1];
+        }
+    });
+    if (token === undefined || token === null || token === '' || localStorage.getItem('user') === null || localStorage.getItem('user') === undefined || JSON.parse(localStorage.getItem('user')).role !== role) {
+        window.location.href = "/WebS2023_war/login";
+        return;
+    }
+    const main = document.getElementById('main');
+    main.innerHTML = `Hello ${localStorage.getItem('user')}`;
+}
+
+checkLogged();
