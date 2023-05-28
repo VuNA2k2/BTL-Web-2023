fe86c2b02b1877f70018a77fa5d3329c72317d0a
VuNAP <VuNAProPTIT@gmail.com>
2023-05-28 14:30:52 +0700

    [EDIT] list user

--- WebS2023/src/main/webapp/admin/user/index.jsp
+++ WebS2023/src/main/webapp/admin/user/index.jsp
@@ -1,17 +1,93 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: VuNAP
-  Date: 5/22/2023
-  Time: 7:20 PM
-  To change this template use File | Settings | File Templates.
---%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
 <html>
 <head>
-    <title>Quản lý người dùng</title>
+    <meta charset="UTF-8">
+    <title>User Management</title>
+    <style>
+        .user-card {
+            border: 1px solid #ccc;
+            padding: 10px;
+            margin-bottom: 10px;
+            cursor: pointer;
+        }
+
+        #popup {
+            display: none;
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, 0.5);
+            align-items: center;
+            justify-content: center;
+        }
+
+        #popup-content {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            background-color: white;
+            padding: 20px;
+            border-radius: 5px;
+            width: 400px;
+        }
+
+        #popup-content h2 {
+            margin-top: 0;
+        }
+
+        #popup-content label {
+            display: block;
+            margin-bottom: 5px;
+        }
+
+        #popup-content input[type="text"],
+        #popup-content input[type="email"],
+        #popup-content input[type="password"] {
+            width: 100%;
+            padding: 5px;
+            margin-bottom: 10px;
+        }
+
+        #popup-content button {
+            margin-top: 10px;
+        }
+    </style>
 </head>
 <body>
-<h1>Quản lý người dùng</h1>
+<%@include file="../../layout/header.jsp" %>
+<h1>User Management</h1>
+<div id="users-container"></div>
+
+<div id="popup">
+    <div id="popup-content">
+        <h2>User Details</h2>
+        <form id="user-form">
+            <input type="hidden" id="id">
+            <label for="username">Username:</label>
+            <input type="text" id="username" required>
+            <label for="password">Password:</label>
+            <input type="password" id="password" required>
+            <label for="fullName">Full Name:</label>
+            <input type="text" id="fullName" required>
+            <label for="email">Email:</label>
+            <input type="email" id="email" required>
+            <label for="phone">Phone:</label>
+            <input type="text" id="phone" required>
+            <label for="address">Address:</label>
+            <input type="text" id="address" required>
+            <label for="role">Role:</label>
+            <input type="text" id="role" required>
+            <button type="submit">Save</button>
+            <button type="button" id="delete-button">Delete</button>
+        </form>
+    </div>
+</div>
+
+<button onclick="showAddUserForm()">Add User</button>
+<%@include file="../../layout/footer.jsp" %>
 <script src="script.js"></script>
 </body>
 </html>
--- WebS2023/src/main/webapp/admin/user/script.js
+++ WebS2023/src/main/webapp/admin/user/script.js
@@ -14,3 +14,144 @@ function checkLogged(role) {
 }
 
 checkLogged('ADMIN');
+
+const users = [
+    {
+        "id": 1,
+        "username": "john_doe",
+        "password": "123456",
+        "full_name": "John Doe",
+        "email": "john.doe@example.com",
+        "phone": "123456789",
+        "address": "123 Main St",
+        "role": "USER"
+    },
+    {
+        "id": 2,
+        "username": "jane_smith",
+        "password": "abcdef",
+        "full_name": "Jane Smith",
+        "email": "jane.smith@example.com",
+        "phone": "987654321",
+        "address": "456 Elm St",
+        "role": "ADMIN"
+    }
+];
+
+document.addEventListener('DOMContentLoaded', getUsers);
+
+function getUsers() {
+    const usersContainer = document.getElementById('users-container');
+    usersContainer.innerHTML = '';
+
+    users.forEach(user => {
+        const userCard = document.createElement('div');
+        userCard.classList.add('user-card');
+        userCard.setAttribute('data-userid', user.id);
+        userCard.innerHTML = `
+            <p><strong>ID:</strong> ${user.id}  <strong>Username:</strong> ${user.username}  <strong>Role:</strong> ${user.role}</p>
+        `;
+        userCard.addEventListener('click', () => showUserDetails(user.id));
+        usersContainer.appendChild(userCard);
+    });
+}
+
+function showUserDetails(userId) {
+    const user = users.find(user => user.id === userId);
+
+    const popup = document.getElementById('popup');
+    const form = document.getElementById('user-form');
+    const idInput = document.getElementById('id');
+    const usernameInput = document.getElementById('username');
+    const passwordInput = document.getElementById('password');
+    const fullNameInput = document.getElementById('fullName');
+    const emailInput = document.getElementById('email');
+    const phoneInput = document.getElementById('phone');
+    const addressInput = document.getElementById('address');
+    const roleInput = document.getElementById('role');
+
+    idInput.value = user.id;
+    usernameInput.value = user.username;
+    passwordInput.value = user.password;
+    fullNameInput.value = user.full_name;
+    emailInput.value = user.email;
+    phoneInput.value = user.phone;
+    addressInput.value = user.address;
+    roleInput.value = user.role;
+
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+
+        const updateUser = {
+            id: idInput.value,
+            username: usernameInput.value,
+            password: passwordInput.value,
+            full_name: fullNameInput.value,
+            email: emailInput.value,
+            phone: phoneInput.value,
+            address: addressInput.value,
+            role: roleInput.value
+        };
+
+        const index = users.findIndex(user => user.id === userId);
+        users[index] = updateUser;
+
+        popup.style.display = 'none';
+        getUsers();
+    });
+
+    const deleteButton = document.getElementById('delete-button');
+    deleteButton.addEventListener('click', () => {
+        const index = users.findIndex(user => user.id === userId);
+        users.splice(index, 1);
+
+        popup.style.display = 'none';
+        getUsers();
+    });
+
+    popup.style.display = 'block';
+}
+
+function showAddUserForm() {
+    const popup = document.getElementById('popup');
+    const form = document.getElementById('user-form');
+    const idInput = document.getElementById('id');
+    const usernameInput = document.getElementById('username');
+    const passwordInput = document.getElementById('password');
+    const fullNameInput = document.getElementById('fullName');
+    const emailInput = document.getElementById('email');
+    const phoneInput = document.getElementById('phone');
+    const addressInput = document.getElementById('address');
+    const roleInput = document.getElementById('role');
+
+    idInput.value = '';
+    usernameInput.value = '';
+    passwordInput.value = '';
+    fullNameInput.value = '';
+    emailInput.value = '';
+    phoneInput.value = '';
+    addressInput.value = '';
+    roleInput.value = '';
+
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+
+        const newUser = {
+            id: users.length + 1,
+            username: usernameInput.value,
+            password: passwordInput.value,
+            full_name: fullNameInput.value,
+            email: emailInput.value,
+            phone: phoneInput.value,
+            address: addressInput.value,
+            role: roleInput.value
+        };
+
+        users.push(newUser);
+
+        popup.style.display = 'none';
+        getUsers();
+    });
+
+    popup.style.display = 'block';
+}
