99432b8e882e3b09bbdfcba9ce4603478d3de10f
kienkienit <kienkienit268@gmail.com>
2023-06-03 23:46:04 +0700

    update comment

--- WebS2023/src/main/webapp/comment/index.jsp
+++ WebS2023/src/main/webapp/comment/index.jsp
@@ -1,16 +1,36 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: memor
-  Date: 5/29/2023
-  Time: 11:24 PM
-  To change this template use File | Settings | File Templates.
---%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
 <html>
 <head>
-    <title>Title</title>
+    <meta charset="UTF-8">
+    <title>Product Reviews</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-
+<h1>Product Reviews</h1>
+<div id="product-info">
+    <p>Average Rating: <span id="avg-rates"></span></p>
+    <p>Total Reviews: <span id="count-rates"></span></p>
+</div>
+<ul id="review-list"></ul>
+<form id="review-form">
+    <input type="hidden" id="product-id-hidden" value="12">
+    <div>
+        <label for="comment">Comment:</label>
+<%--        <textarea id="comment" rows="4" cols="50"></textarea>--%>
+        <input type="text" id="comment"></div>
+    </div>
+    <div>
+        <label for="star">Rating:</label>
+        <select id="star">
+            <option value="1">1 Star</option>
+            <option value="2">2 Stars</option>
+            <option value="3">3 Stars</option>
+            <option value="4">4 Stars</option>
+            <option value="5">5 Stars</option>
+        </select>
+    </div>
+    <button type="submit" onclick="submitReview()">Submit Review</button>
+</form>
+<script src="script.js"></script>
 </body>
 </html>
---
+++ WebS2023/src/main/webapp/comment/script.js
@@ -0,0 +1,127 @@
+// Lấy token từ cookie
+function getTokenFromCookie() {
+    const cookie = document.cookie.split(';');
+    const token = cookie[0].substring("token=".length, cookie[0].length);
+    return token;
+}
+
+// Lấy đánh giá sản phẩm theo productId
+function getProductReviews(productId) {
+    const url = `https://localhost:443/WebS2023_war/api/rates/avg?productId=${productId}`;
+
+    const token = getTokenFromCookie();
+    const headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        // 'Authorization': `Bearer ${token}`,
+    };
+
+    fetch(url, {
+        headers: headers,
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.code === 'success') {
+                const avgRates = data.data.avgRate;
+                const countRates = data.data.countRate;
+
+                displayProductInfo(avgRates, countRates);
+            } else {
+                console.log('Lỗi lấy đánh giá sản phẩm');
+            }
+        })
+        .catch(error => {
+            console.log('Error connect to API: ', error);
+        });
+    fetch(`https://localhost:443/WebS2023_war/api/rates?productId=${productId}`, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            // 'Authorization': 'Bearer ' + getTokenFromCookie(),
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.code === 'success') {
+
+                const reviews = data.data;
+                displayProductReviews(reviews)
+            } else {
+                console.log('Lỗi lấy đánh giá sản phẩm');
+            }
+        })
+        .catch(error => {
+            console.log('Error connect to API: ', error);
+        });
+}
+
+
+// Hiển thị thông tin sản phẩm
+function displayProductInfo(avgRates, countRates) {
+    document.getElementById('avg-rates').textContent = avgRates.toFixed(1);
+    document.getElementById('count-rates').textContent = countRates;
+}
+
+// Hiển thị danh sách đánh giá sản phẩm
+function displayProductReviews(reviews) {
+    const reviewList = document.getElementById('review-list');
+    reviewList.innerHTML = '';
+
+    reviews.forEach(review => {
+        const reviewItem = document.createElement('li');
+        reviewItem.className = 'review-item';
+        reviewItem.innerHTML = `
+            <p>Comment: ${review.comment}</p>
+            <p>Rating: ${review.star}</p>
+            <p>Created At: ${review.createdAt}</p>
+        `;
+        reviewList.appendChild(reviewItem);
+    });
+}
+
+// Gửi đánh giá lên api
+function submitReview() {
+    const productId = parseInt(document.getElementById('product-id-hidden').value);
+    const comment = document.getElementById('comment').value;
+    const star = parseInt(document.getElementById('star').value);
+    const url = 'https://localhost:443/WebS2023_war/api/rates?';
+
+    const token = getTokenFromCookie();
+    const headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': 'Bearer ' + token,
+    };
+
+    const data = {
+        comment: comment,
+        star: parseInt(star),
+        productInOrderId: parseInt(productId),
+        images: [],
+    };
+
+    fetch(url, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(data)
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.code === 'success') {
+                // Gửi thành công, cập nhật lại danh sách đánh giá
+                getProductReviews(productId);
+                document.getElementById('comment').value = '';
+                document.getElementById('star').value = '';
+            } else {
+                console.log('Lỗi gửi đánh giá');
+            }
+        })
+        .catch(error => {
+            console.log('Lỗi kết nối API: ', error);
+        });
+}
+
+// Khi tải xong trang, lấy danh sách đánh giá cho sản phẩm
+window.onload = function() {
+    getProductReviews(2);
+}
