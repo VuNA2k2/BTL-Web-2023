2e0998d7f9d1f0a27c6321f90959cb354e82d02b
VuNAP <VuNAProPTIT@gmail.com>
2023-06-09 08:24:26 +0700

    create cart after register user

--- WebS2023/src/main/java/com/example/webs2023/service/auth/AuthServiceImpl.java
+++ WebS2023/src/main/java/com/example/webs2023/service/auth/AuthServiceImpl.java
@@ -5,6 +5,7 @@ import com.example.webs2023.dto.login.LoginInput;
 import com.example.webs2023.dto.login.LoginOutput;
 import com.example.webs2023.dto.user.UserInput;
 import com.example.webs2023.dto.user.UserOutput;
+import com.example.webs2023.service.cart.CartService;
 import com.example.webs2023.service.jwt.JwtService;
 import com.example.webs2023.service.user.UserService;
 
@@ -17,9 +18,12 @@ public class AuthServiceImpl implements AuthService {
     UserService userService;
     JwtService jwtService;
 
-    public AuthServiceImpl(UserService userService, JwtService jwtService) {
+    CartService cartService;
+
+    public AuthServiceImpl(UserService userService, JwtService jwtService, CartService cartService) {
         this.userService = userService;
         this.jwtService = jwtService;
+        this.cartService = cartService;
     }
 
     public LoginOutput login(LoginInput loginInput) throws SQLException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException, NoSuchAlgorithmException, InvalidKeyException {
@@ -35,6 +39,8 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public UserOutput register(UserInput userInput) throws SQLException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
-        return userService.saveUser(userInput);
+        UserOutput userOutput = userService.saveUser(userInput);
+        cartService.createCart(userOutput.getId());
+        return userOutput;
     }
 }
