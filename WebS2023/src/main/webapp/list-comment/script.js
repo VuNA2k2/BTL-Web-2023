01e1bb74b843e7bedea4bef7c235cbb7215f658c
kienkienit <kienkienit268@gmail.com>
2023-06-11 01:56:04 +0700

    update show images in list-comment

--- WebS2023/src/main/webapp/list-comment/script.js
+++ WebS2023/src/main/webapp/list-comment/script.js
@@ -1,6 +1,6 @@
 // Lấy productId
 const urlParamsProduct = new URLSearchParams(window.location.search);
-const productId = parseInt(urlParamsProduct.get("productId"));
+const productId = parseInt(urlParamsProduct.get('productId'));
 console.log(productId);
 
 // Lấy đánh giá sản phẩm theo productId
@@ -25,7 +25,6 @@ function getListProductReviews() {
         .catch(error => {
             console.log('Error connect to API: ', error);
         });
-
     fetch(`https://localhost:443/WebS2023_war/api/rates?productId=${productId}`, {
         headers: {
             'Content-Type': 'application/json',
@@ -46,7 +45,7 @@ function getListProductReviews() {
         });
 }
 
-// Hiển thị đánh giá sản phẩm
+// Hiển thị đánh giá chung về sản phẩm
 function displayListProductInfo(avgRates, countRates) {
     document.getElementById('avg-rates').textContent = avgRates.toFixed(1) + "/5.0 ";
     document.getElementById('count-rates').textContent = countRates;
@@ -56,15 +55,17 @@ function displayListProductInfo(avgRates, countRates) {
 function displayListProductReviews(reviews) {
     const reviewListProduct = document.getElementById('review-list');
     reviewListProduct.innerHTML = '';
-
     reviews.forEach(review => {
+        console.log(review);
         const reviewItems = document.createElement('li');
         reviewItems.className = 'review-item';
         let imagesHtml = '';
         if (review.images.length > 0) {
             imagesHtml = '<div class="review-images">';
             review.images.forEach(image => {
-                imagesHtml += `<img src="${image}" alt="Review Image">`;
+                const aspectRatio = 150 / image.height;
+                const newWidth = Math.round(image.width * aspectRatio);
+                imagesHtml += `<img src="${image.link}" alt="Review Image" style="max-height: 150px; width: ${newWidth}px;">`;
             });
             imagesHtml += '</div>';
         }
